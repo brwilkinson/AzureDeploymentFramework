765568099a4d396a3f55b16ea25a3e3d4b6a6af0
Ben Wilkinson <benwilk@microsoft.com>
2021-02-16 14:00:12 -0800

    update apim for certs via managed identity

--- ADF/tenants/AOA/azure-Deploy.ps1
+++ ADF/tenants/AOA/azure-Deploy.ps1
@@ -1,7 +1,7 @@
 # F5 to load
 $ASD = Get-Item -Path "$PSScriptRoot\..\.."
 $App = 'AOA'
-$Enviro = 'P0'
+$Enviro = 'S1'
 # import deployment script
 if(!(test-path ASD:\)){new-psdrive -PSProvider FileSystem -Root $ASD -Name ASD}
 . ASD:\release-az\Start-AzDeploy.ps1
