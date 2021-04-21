d0462aa93e6b39ada904867d8d97a6b80309b761
Ben Wilkinson <benwilk@microsoft.com>
2021-04-21 09:48:08 -0700

    - resolve azure front door misconfiguration on reference to frontdoor resourceid - add script to cleanup dangling dns records, need to add other resource types here

---
+++ ADF/1-PrereqsToDeploy/26-CleanupDNSRecords.ps1
@@ -0,0 +1,5 @@
+# cleanup FrontDoor Dangling DNS
+$fdname = 'ACU1-BRW-AOA-S1-afd01.azurefd.net'
+Get-AzDnsRecordSet -ResourceGroupName ACU1-BRW-AOA-RG-G1 -ZoneName 'psthing.com' -RecordType CNAME | 
+    where {$_.records[0].cname -match $fdname} | 
+    Remove-AzDnsRecordSet
