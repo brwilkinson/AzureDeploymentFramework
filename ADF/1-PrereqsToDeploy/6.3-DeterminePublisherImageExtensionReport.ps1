8298b496058dd85ed9ba9d10496f2df92ba51bfe
Ben Wilkinson <benwilk@microsoft.com>
2021-10-10 12:23:45 -0700

    add report on extension versions

--- ADF/1-PrereqsToDeploy/6.3-DeterminePublisherImageExtensionReport.ps1
+++ ADF/1-PrereqsToDeploy/6.3-DeterminePublisherImageExtensionReport.ps1
@@ -11,8 +11,8 @@ Get-AzVM -ResourceGroupName ACU1-BRW-AOA-RG-P0 | ForEach-Object {
         $vme = $_
         $latest = Get-AzVMExtensionImage -Location $vme.Location -PublisherName $vme.Publisher -Type $vme.ExtensionType |
             Sort-Object -Descending -Property { [version]$_.version } -Top 1
-            $extensionLatest[$_.Publisher + '.' + $_.ExtensionType] = $latest.version
-        }
+        $extensionLatest[$_.Publisher + '.' + $_.ExtensionType] = $latest.version
+    }
 
     Invoke-AzRestMethod -Path ($vm.id + '/instanceView?api-version=2017-03-30') -Method GET |
         ForEach-Object content | ConvertFrom-Json | ForEach-Object vmagent | 
