18434ecf83635b36519fff3209a0a76b25f87156
brwilkinson <brwilkinson@outlook.com>
2023-03-16 14:53:13 -0700

    update info for public cert providers

--- ADF/bicep/loadTextContext/setCertificateIssuer.ps1
+++ ADF/bicep/loadTextContext/setCertificateIssuer.ps1
@@ -19,3 +19,20 @@ else
     Set-AzKeyVaultCertificateIssuer -VaultName $VaultName -Name $CertIssuerName -IssuerProvider $CertIssuerProvider -PassThru
 }
 
+# https://learn.microsoft.com/en-us/azure/key-vault/certificates/how-to-integrate-certificate-authority#before-you-begin
+
+<#
+    $AdminDetails = New-AzKeyVaultCertificateAdministratorDetail -FirstName user -LastName name -EmailAddress username@microsoft.com
+    $OrgDetails = New-AzKeyVaultCertificateOrganizationDetail -AdministratorDetails $AdminDetails
+    $Password = ConvertTo-SecureString -String P@ssw0rd -AsPlainText -Force
+    Set-AzKeyVaultCertificateIssuer -VaultName "Contosokv01" -Name "TestIssuer01" -IssuerProvider "Test" -AccountId "555" -ApiKey $Password -OrganizationDetails $OrgDetails -PassThru
+
+    AccountId           : 555
+    ApiKey              :
+    OrganizationDetails : Microsoft.Azure.Commands.KeyVault.Models.PSKeyVaultCertificateOrganizationDetails
+    Name                : TestIssuer01
+    IssuerProvider      : Test
+    VaultName           : Contosokv01
+
+    This command sets the properties for a certificate issuer.
+#>
