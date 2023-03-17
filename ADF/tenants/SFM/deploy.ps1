4761c6a0fc1f6cf17d9dc053b97efd35119d6f87
brwilkinson <brwilkinson@outlook.com>
2023-03-16 18:50:50 -0700

    update SFM to deploy D1

--- ADF/tenants/SFM/deploy.ps1
+++ ADF/tenants/SFM/deploy.ps1
@@ -1,100 +1,33 @@
 param (
     [string]$App = 'SFM'
 )
-Import-Module -Name "$PSScriptRoot\..\..\release-az\azSet.psm1" -Force
-Import-Module -Name "$PSScriptRoot\..\..\release-az\ADOHelper.psm1" -Force
+$Base = $PSScriptRoot
+Import-Module -Name "$Base/../../release-az/azSet.psm1" -Force
+Import-Module -Name "$Base/../../release-az/ADOHelper.psm1" -Force
 # 1) Set Deployment information
 AzSet -App $App -Enviro D1
 break
 # F8 to run individual steps
 
-# Export all role defintions per Subscription, only needed 1 time or when new roles added 
+# Export all role defintions per Subscription, only needed 1 time or when new roles added
 . ADF:\1-prereqs\04.1-getRoleDefinitionTable.ps1 @Current
 
-# ALT account for PROD on SAW
-
-# need access to DevOpsPatToken
-getpim -Resource ACU1-PE-HUB-RG-P0 | setpim -duration PT8H
-
 # App pipelines in AZD
-New-ADOAZServiceConnection -Prefix ACU1 -App $App -Environments D1, U5, P8
-New-ADOAZServiceConnection -Prefix AEU2 -App $App -Environments P8
+New-ADOAZServiceConnection -Prefix ACU1 -App $App -IncludeReaderOnSubscription -Environments D1 #, U5, P8
+# New-ADOAZServiceConnection -Prefix AEU2 -App $App -IncludeReaderOnSubscription -Environments P8
 
-New-ADOAZServiceConnection -Prefix ACU1 -App $App -Suffix '_SFM' -Environments D1, U5, P8
-New-ADOAZServiceConnection -Prefix AEU2 -App $App -Suffix '_SFM' -Environments P8
-
-# update secrets
-Set-ADOAZServiceConnection -Prefix ACU1 -App $App -RenewDays 360 -Environments D1, U5, P8
-Set-ADOAZServiceConnection -Prefix AEU2 -App $App -RenewDays 360 -Environments P8
-
-Set-ADOAZServiceConnection -Prefix ACU1 -App $App -Suffix '_SFM' -RenewDays 360 -Environments D1, U5, P8
-Set-ADOAZServiceConnection -Prefix AEU2 -App $App -Suffix '_SFM' -RenewDays 360 -Environments P8
-
-# Moved this to manually run for Owner assignment
-$IDs = @(
-    # 'todo' # SP GO
-    'todo' # uaiGlobal PrincipalId
-)
-$Scope = '/subscriptions/{TODO-AddSubscriptionId}'
-$IDs | ForEach-Object {
-    $r = Get-AzRoleAssignment -Scope $Scope -ObjectId $_ -RoleDefinitionName Owner
-    if ($r)
-    {
-        $r
-    }
-    else
-    {
-        New-AzRoleAssignment -Scope $Scope -ObjectId $_ -RoleDefinitionName Owner
-    }
-}
-
-# Moved this to manually run for Reader assignment, to be run on SAW device.
-$IDs = @(
-    'todo',
-    'todo'
-)
-$Scope = '/subscriptions/{TODO-AddSubscriptionId}'
-$IDs | ForEach-Object {
-    $r = Get-AzRoleAssignment -Scope $Scope -ObjectId $_ -RoleDefinitionName Reader
-    if ($r)
-    {
-        $r
-    }
-    else
-    {
-        New-AzRoleAssignment -Scope $Scope -ObjectId $_ -RoleDefinitionName Reader
-    }
-}
-
-# add service principals for SF app deployment
-New-ADOAZServiceConnection -APP $App -Prefix ACU1 -Suffix '_SFM' -Environments D1, U5, P8
-New-ADOAZServiceConnection -APP $App -Prefix AEU2 -Suffix '_SFM' -Environments P8
+New-ADOAZServiceConnection -Prefix ACU1 -App $App -Suffix '_SFM' -Environments D1 #, U5, P8
+# New-ADOAZServiceConnection -Prefix AEU2 -App $App -Suffix '_SFM' -Environments P8
 
 # update secrets
-Set-ADOAZServiceConnection -App $App -Prefix ACU1 -Suffix '_SFM' -Environments D1, U5 -RenewDays 400
+Set-ADOAZServiceConnection -Prefix ACU1 -App $App -RenewDays 360 -Environments D1 #, U5, P8
+# Set-ADOAZServiceConnection -Prefix AEU2 -App $App -RenewDays 360 -Environments P8
+
+Set-ADOAZServiceConnection -Prefix ACU1 -App $App -Suffix '_SFM' -RenewDays 360 -Environments D1 #, U5, P8
+# Set-ADOAZServiceConnection -Prefix AEU2 -App $App -Suffix '_SFM' -RenewDays 360 -Environments P8
 
 ##########################################################
 # Deploy Environment
-
-# 1) Set Deployment information - Subscription
-AzSet -App $App -Enviro G0
-
-# Global - Only Needed in primary Region (for subscription deployment)
-AzDeploy @Current -Prefix ACU1 -TF ADF:\bicep\00-ALL-SUB.bicep
-
-# 2) Set Deployment information - Optional Global
-AzSet -App $App -Enviro G0
-
-# Global - Only Needed in primary Region
-AzDeploy @Current -Prefix ACU1 -TF ADF:\bicep\00-ALL-SUB.bicep
-AzDeploy @Current -Prefix ACU1 -TF ADF:\bicep\01-ALL-RG.bicep
-
-# 3) Set Deployment information - Hub
-AzSet -App $App -Enviro P0
-
-# Global - Only Needed in primary Region
-AzDeploy @Current -Prefix ACU1 -TF ADF:\bicep\00-ALL-SUB.bicep
-AzDeploy @Current -Prefix ACU1 -TF ADF:\bicep\01-ALL-RG.bicep
 
 # 4) Set Deployment information - Dev Environment
 AzSet -App $App -Enviro D1
@@ -102,5 +35,8 @@ AzSet -App $App -Enviro D1
 # Global - Only Needed in secondary Region
 AzDeploy @Current -Prefix ACU1 -TF ADF:\bicep\00-ALL-SUB.bicep
 AzDeploy @Current -Prefix ACU1 -TF ADF:\bicep\01-ALL-RG.bicep
+
+# Deploy only AKS layer for testing.
+AzDeploy @Current -Prefix ACU1 -TF ADF:\bicep\SFM.bicep
 
 # Repeat above for other environments, however can do those in yaml pipelines instead
