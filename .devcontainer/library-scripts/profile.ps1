46abbb8d11aa7888f0839e3d4960fc6f1fa44504
brwilkinson <brwilkinson@outlook.com>
2023-05-09 01:59:01 +0000

    update psreadline options

--- .devcontainer/library-scripts/profile.ps1
+++ .devcontainer/library-scripts/profile.ps1
@@ -2,3 +2,4 @@ Write-Warning -Message 'I am user PowerShell'
 Import-Module posh-git, Terminal-Icons
 & ([ScriptBlock]::Create((oh-my-posh init pwsh --print) -join "`n"))
 Set-PSReadLineOption -HistorySavePath /home/vscode/PowerShell_PSReadLine_History/ConsoleHost_history.txt
+Set-PSReadLineOption -PredictionViewStyle ListView -PredictionSource HistoryAndPlugin
